55d85a4bceeb1c19951b7f81fe2f994668ecffea
IvanM <ivan.montes@outlook.com>
2023-10-06 21:15:55 +0200

    Add pom

---
+++ src/main/java/ime/mskafkaprodcon/MsKafkaProdConApplication.java
@@ -0,0 +1,13 @@
+package ime.mskafkaprodcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsKafkaProdConApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsKafkaProdConApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ime/mskafkaprodcon/MsKafkaProdConApplicationTests.java
@@ -0,0 +1,13 @@
+package ime.mskafkaprodcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsKafkaProdConApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
