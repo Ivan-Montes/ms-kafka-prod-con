be3addb28f88d665533753df4ee15ac8f149ace6
IvanM <ivan.montes@outlook.com>
2023-10-07 13:32:29 +0200

    Add main boot class

---
+++ kafka-prod/src/main/java/mod/prod/SpringBootModuleProducer.java
@@ -0,0 +1,13 @@
+package mod.prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModuleProducer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModuleProducer.class);
+	}
+	
+}
