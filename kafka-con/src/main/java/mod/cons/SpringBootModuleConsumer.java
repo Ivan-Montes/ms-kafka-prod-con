5aa438995aef5a96b218e71619acc8be9eba87d5
IvanM <ivan.montes@outlook.com>
2023-10-11 16:03:08 +0200

    Add Kafka module

---
+++ kafka-con/src/main/java/mod/cons/SpringBootModuleConsumer.java
@@ -0,0 +1,16 @@
+package mod.cons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootModuleConsumer {
+
+	public static void main(String [] args) {
+		
+		SpringApplication.run(SpringBootModuleConsumer.class);
+		
+	}
+	
+}
